56ec3d478db3b0ebd7493682fb5362dea3a73b89
朱育茗 <sjfhzhuyuming@outlook.com>
2024-04-09 15:12:59 +1000

    Append url group to settings

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data-fetch/', include('data_fetch.urls')),
 ]
