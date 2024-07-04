2972aea9ffca8fccf6ac589d322271640bf52467
Yuming Zhu <89754809+yuming-z@users.noreply.github.com>
2024-07-04 13:51:00 +0800

    Create date range validation function

--- mysite/website/views.py
+++ mysite/website/views.py
@@ -29,6 +29,39 @@ def is_exist_ticker(ticker_code):
         return HttpResponse(status=404, content="Ticker not found.")
     return ticker_data
 
+def is_valid_date_range(start_date, end_date, ticker: Ticker):
+    '''
+    This function is used to validate the date range for the stock data.
+
+    Parameters:
+    - start_date: str
+    - end_date: str
+    - ticker: Ticker
+
+    Exceptions:
+    - If the date format is invalid
+    - If the start date is earlier than the earliest date in record
+    '''
+
+    # Convert the dates to datetime objects
+    start_date = dateparse.parse_date(start_date)
+    end_date = dateparse.parse_date(end_date)
+
+    if start_date is None or end_date is None:
+        return HttpResponse(status=400, content="Invalid date format.")
+
+    # Check if the start date exists in record
+    earlies_start_date = Stock.objects.filter(ticker=ticker.id).order_by('date').first().date
+    if start_date < earlies_start_date:
+        return HttpResponse(status=400, content="Start date is earlier than the earliest date in record.")
+
+    # Check if the end date exists the maximum end date in record
+    # If the end date exceeds the maximum end date, change the end date to the maximum end date
+    max_end_date = Stock.objects.filter(ticker=ticker.id).order_by('-date').first().date
+    if end_date > max_end_date:
+        print("End date is later than the latest date in record. The end date is changed to the latest date in record.")
+        end_date = max_end_date
+
 @api_view(['GET'])
 def validate(request):
     '''This function is used to validate the connection to the database.'''
@@ -46,14 +79,8 @@ def query_ticker(request):
     # Get the parameter value
     ticker_code = request.GET.get('ticker')
 
-    # Switch the ticker code to uppercase
-    ticker_code = ticker_code.upper()
-
-    # Query the database for the ticker data
-    try:
-        ticker_data = get_object_or_404(Ticker, ticker=ticker_code)
-    except Http404:
-        return HttpResponse(status=404, content="Ticker not found.")
+    # Get the ticker object
+    ticker_data = is_exist_ticker(ticker_code)
 
     # Serialize the data
     response = TickerSerializer(ticker_data, many=True)
@@ -72,32 +99,11 @@ def query_stock(request):
     start_date = request.GET.get('startDate')
     end_date = request.GET.get('endDate')
 
-    # Switch the ticker code to uppercase
-    ticker_code = ticker_code.upper()
+    # Get the Ticker object
+    ticker = is_exist_ticker(ticker_code)
 
-    # Change the dates to datetime objects
-    start_date = dateparse.parse_date(start_date)
-    end_date = dateparse.parse_date(end_date)
-
-    if start_date is None or end_date is None:
-        return HttpResponse(status=400, content="Invalid date format.")
-
-    # Find the ticker id
-    try:
-        ticker = get_object_or_404(Ticker, ticker=ticker_code)
-    except Http404:
-        return HttpResponse(status=404, content="Ticker not found.")
-
-    # Check if the start date exists in record
-    earlies_start_date = Stock.objects.filter(ticker=ticker.id).order_by('date').first().date
-    if start_date < earlies_start_date:
-        return HttpResponse(status=400, content="Start date is earlier than the earliest date in record.")
-
-    # Check if the end date exists the maximum end date in record
-    max_end_date = Stock.objects.filter(ticker=ticker.id).order_by('-date').first().date
-    if end_date > max_end_date:
-        print("End date is later than the latest date in record. The end date is changed to the latest date in record.")
-        end_date = max_end_date
+    # Validate the date range
+    is_valid_date_range(start_date, end_date, ticker)
 
     # Query the database for the ticker data
     ticker_data = Stock.objects.filter(ticker=ticker.id, date__range=[start_date, end_date])
@@ -127,3 +133,9 @@ def query_stock_analytic_data(request):
     ticker_code = request.GET.get('ticker')
     start_date = request.GET.get('startDate')
     end_date = request.GET.get('endDate')
+
+    # Get the Ticker object
+    ticker = is_exist_ticker(ticker_code)
+
+    # Validate the date range
+    is_valid_date_range(start_date, end_date, ticker)
