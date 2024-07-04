6e06447dcbd78787f304fbeff54f64091c35b9fa
Yuming Zhu <89754809+yuming-z@users.noreply.github.com>
2024-07-04 15:28:42 +0800

    Update the stock data loading script to include analytic metrics

--- mysite/import_data.py
+++ mysite/import_data.py
@@ -8,15 +8,17 @@ import glob
 import pandas as pd
 from website.models import Ticker, Stock
 
+DATASET_DIR = 'ProcessedHLOCData/'
+
 # Get all csv files in the folder
 path = os.getcwd()
-csv_files = glob.glob(os.path.join(path, "HLOCData/","*.csv"))
+csv_files = glob.glob(os.path.join(path, DATASET_DIR,"*.csv"))
 
 # Loop through all csv files
 for file in csv_files:
 
     # Get the ticker name from csv file name
-    ticker_name = os.path.basename(file).split(".")[0]
+    ticker_name = os.path.basename(file).split(".")[0].strip('_processed')
 
     # Print start message
     print(f"Importing data for {ticker_name}...")
@@ -30,7 +32,10 @@ for file in csv_files:
     ticker.save()
 
     # Read the csv file
-    data = pd.read_csv(file)
+    data = pd.read_csv(file, parse_dates=['Date'])
+
+    # Set the index to date
+    data.set_index('Date', inplace=True)
 
     # Loop through all rows in the csv file
     for index, row in data.iterrows():
@@ -45,14 +50,38 @@ for file in csv_files:
             low=row["Low"],
             volume=row["Volume"],
             dividend=row["Dividends"],
-            split=row["Stock Splits"]
+            split=row["Stock Splits"],
+
+            # Analytic metrics
+            sma=row["SMA"],
+            sd=row["SD"],
+            ub=row["UB"],
+            lb=row["LB"],
+            macd=row["MACD"],
+            macd_signal=row["MACDSignal"],
+            gain=row["gain"],
+            loss=row["loss"],
+            avg_gain=row["avg_gain"],
+            avg_loss=row["avg_loss"],
+            rs=row["RS"],
+            rsi=row["RSI"],
+            rsi_6=row["RSI6"],
+            rsi_12=row["RSI12"],
+            weakMACD=row["WeakMACD"],
+            macd_diff=row["MACDDiff"],
+            
+            isLoss=row["Loss"],
+            isCausion=row["Causion"],
+            isSafe=row["Safe"],
+            isGain=row["Gain"],
+            flag=row["flag"]
         )
 
         # Save the stock
         stock.save()
 
     # Print success message
-    print(f"Successfully imported data for {ticker_name}")
+    print(f"Successfully import data for {ticker_name}")
 
 # Print completion message
 print("Data import complete.")
